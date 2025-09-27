0692e6964dcb990e2ee74ac07be9441877269383
Léo Liu <leoliuhy@gmail.com>
2025-09-27 13:46:22 -0400

    Initial Commit

---
+++ AutoVolume/AutoVolumeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoVolumeApp.swift
+//  AutoVolume
+//
+//  Created by Leo Liu on 2025-09-27.
+//
+
+import SwiftUI
+
+@main
+struct AutoVolumeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
